d49d72237bbe3708a07132fb5adcd5a0e85cc9af
JeromePrevotat <jer.prevotat@gmail.com>
2025-05-05 10:17:10 +0200

    feature: main test fix: null List toString

--- ElectricityBuisiness/src/model/LieuRecharge.java
+++ ElectricityBuisiness/src/model/LieuRecharge.java
@@ -59,7 +59,9 @@ public class LieuRecharge {
         sb.append("=== Lieu de Recharge ").append(this.id).append(" ===").append("\n")
             .append("Nom: ").append(this.nom).append("\n")
             .append("Adresse: ").append(this.adresse).append("\n")
-            .append("Bornes: ").append(this.bornes.toString()).append("\n");
+            .append("Bornes: ").append(
+                (this.bornes == null) ? null : this.bornes.toString()
+            ).append("\n");
         return sb.toString();
     }
 
--- ElectricityBuisiness/src/ui/App.java
+++ ElectricityBuisiness/src/ui/App.java
@@ -1,7 +1,25 @@
 package ui;
 
+import model.BorneRecharge;
+import model.LieuRecharge;
+import model.Reservation;
+import model.Utilisateur;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        tests();
+    }
+    
+    private static void tests(){
+        Utilisateur u0 = new Utilisateur();
+        BorneRecharge b0 = new BorneRecharge();
+        LieuRecharge l0 = new LieuRecharge();
+        Reservation r0 = new Reservation();
 
+        
+        System.out.println("\n" + u0.toString());
+        System.out.println("\n" + b0.toString());
+        System.out.println("\n" + l0.toString());
+        System.out.println("\n" + r0.toString());
     }
 }
